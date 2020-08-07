eab485dc27b1a5b1cb61bd07d5881df870282f02
Luis Medina <luismedina.git@outlook.com>
2020-08-07 10:32:29 -0400

    Fireball sound and fix collision error

--- traps/fireball_projectile.gd
+++ traps/fireball_projectile.gd
@@ -3,16 +3,26 @@ extends KinematicBody
 export var direction: Vector3
 export var speed: float
 
+func _ready():
+	$ShootSound.play()
+
 func _physics_process(delta):
 	move_and_collide(direction * speed * delta)
 
 func _on_area_body_entered(body):
 	direction = Vector3()
+	
+	$Area.set_collision_layer_bit(0, false)
+	$Area.set_collision_mask_bit(0, false)
 	$BallParticles.emitting = false
 	$Tail.emitting = false
 	$DestroyTimer.start()
 	$ExplosionParticles.emitting = true
 	$AnimationPlayer.play("explode")
+	$ExplosionSound.play()
+	
+	if body.has_method("got_hit"):
+		body.got_hit()
 
 func _on_destroy_timer_timeout():
 	call_deferred("queue_free")
