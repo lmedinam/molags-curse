2d80f51cfe556b3cd3e86e229563377a94db00f7
Luis Medina <luismedina.git@outlook.com>
2020-08-10 13:11:57 -0400

    Making prison sections

--- level/level.gd
+++ level/level.gd
@@ -1,7 +1,8 @@
 extends Spatial
 
 func _ready():
-	$Lever.connect("fired", self, "lever_action")
+	pass
+	# $Lever.connect("fired", self, "lever_action")
 
 func lever_action(status: bool): 
 	print_debug("Lever triggered: " + str(status))
