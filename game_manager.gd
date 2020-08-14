7d6a05b710968cb0d7ee5555d6d6a6be1089fadc
Luis Medina <luismedina.git@outlook.com>
2020-08-14 10:40:10 -0400

    Now you can take a screenshot

--- game_manager.gd
+++ game_manager.gd
@@ -19,3 +19,30 @@ func _process(_delta):
 	
 	if (Input.is_action_just_pressed("fullscreen")):
 		OS.window_fullscreen = !OS.window_fullscreen
+
+func _input(event):
+	if event.is_action_pressed("take_screenshot"):
+		var screenshots = Directory.new()
+		screenshots.open("user://")
+		screenshots.make_dir("screenshots")
+		
+		var dir = Directory.new()
+		var file_count = 0
+		
+		dir.open("user://screenshots")
+		dir.list_dir_begin()
+		
+		while true:
+			var file = dir.get_next()
+			
+			if file == "":
+				break
+			elif not file.begins_with("."):
+				file_count += 1
+		
+		dir.list_dir_end()
+		
+		if event.is_action_pressed("take_screenshot"):
+			var capture = get_viewport().get_texture().get_data()
+			capture.flip_y()
+			capture.save_png("user://screenshots/screenshot_"+str(file_count)+".png")
--- init/level_map.gd
+++ init/level_map.gd
@@ -1,6 +1,4 @@
 extends Spatial
 
 func _input(event):
-	if event.is_action_pressed("take_screenshot"):
-		var capture = get_viewport().get_texture().get_data().flip_y()
-		capture.save_png("user://screenshot.png")
+	pass
